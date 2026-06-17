9d6b6c00a233e388c3e64c5f9eb52dc4f120c044
yehya004 <97907804+yehya004@users.noreply.github.com>
2026-06-17 18:53:57 +0000

    Deploying to gh-pages from @ yehya004/saber_academy@86fac368266c2019c40c4380f600d0eebd90d768 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4054197919" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1559862380" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
